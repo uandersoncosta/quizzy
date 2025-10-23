fd5237ee0420df2632d98c499a0bd75ffa8e1710
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-10-23 14:12:54 -0300

    feat: adicionando a entity user no typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'dbQuizzy.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UserModule,
