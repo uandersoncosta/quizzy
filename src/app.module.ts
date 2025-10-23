9a23585814b177ef58bdc41df436c8a84311ed24
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-10-23 12:45:44 -0300

    feat: adicionado o modulo user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
