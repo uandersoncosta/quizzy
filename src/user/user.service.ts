325c196eeec7cf9ac18917d90d14962599fffdd3
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-10-23 16:27:11 -0300

    feat: adicionado o findAll e findOne no service do user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    const user = await this.userRepository.find();
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) throw new NotFoundException('Este Id não existe');
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
