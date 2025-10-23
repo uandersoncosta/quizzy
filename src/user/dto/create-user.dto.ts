5175213c6e3244b1ec9455dffbe922c06bb3e4a8
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-10-23 13:12:51 -0300

    feat: adicionando o dto create e update user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(5)
+  @MaxLength(100)
+  @IsNotEmpty()
+  @ApiProperty({ example: 'joao.costa' })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: 'joao.costa@gmail.com' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @ApiProperty({ example: '123456789' })
+  password: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { IsEmail } from 'class-validator';
+import { Column, CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  Id: number;
+
+  @Column({ length: 100 })
+  Name: string;
+
+  @Column()
+  @IsEmail()
+  email: string
+
+  @Column({length: 255})
+  Password: string;
+
+  @CreateDateColumn()
+  created_at?: Date;
+
+  @UpdateDateColumn()
+  uploaded_at?: Date;
+  
+  @DeleteDateColumn()
+  excluded_at?: Date;
+}
