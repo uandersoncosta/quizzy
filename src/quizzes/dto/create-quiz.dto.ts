bec561143b367396def68799cfaa63ec4b076b7d
TI <PARALAMAS@Quality.br>
2025-10-24 13:28:18 -0300

    feat: adicionando o dto create quiz

--- src/quizzes/dto/create-quiz.dto.ts
+++ src/quizzes/dto/create-quiz.dto.ts
@@ -1,1 +1,26 @@
-export class CreateQuizDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPositive, MaxLength, MinLength } from 'class-validator';
+
+export class CreateQuizDto {
+
+  @IsPositive()
+  @ApiProperty({ example: 1 })
+  userId: number
+
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(150)
+  @ApiProperty({ example: 'DDL, DML, DQL, DCL' })
+  title: string;
+
+  @MaxLength(255)
+  @ApiProperty({ example: 'Criar um quiz sobre funcionalidades dos comandos SQL' })
+  description?: string;
+
+  @IsNotEmpty()
+  @ApiProperty({ example: 'Fácil' })
+  category: string;
+
+  @IsNotEmpty()
+  difficulty: string;
+}
