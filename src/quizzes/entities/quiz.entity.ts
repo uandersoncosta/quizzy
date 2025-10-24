102f8259ab63169b524aa77118a272ec2e30f95c
TI <PARALAMAS@Quality.br>
2025-10-24 12:54:13 -0300

    feat: criando a entity do quiz

--- src/quizzes/entities/quiz.entity.ts
+++ src/quizzes/entities/quiz.entity.ts
@@ -1,6 +1,51 @@
-import { Entity } from "typeorm";
+import { Users } from 'src/user/entities/users.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum Difficulty {
+  FACIL = 'Fácil',
+  MEDIO = 'Médio',
+  DIFICIL = 'Díficil',
+}
 
 @Entity()
 export class Quiz {
-  id: number
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Users, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  userId: number;
+
+  @Column({ length: 150 })
+  title: string;
+
+  @Column({ length: 150 })
+  description: string;
+
+  @Column({ length: 50 })
+  category: string;
+
+  @Column({
+    type: 'enum',
+    enum: Difficulty,
+    default: Difficulty.FACIL,
+  })
+  difficulty: string;
+
+  @Column({ default: true })
+  is_active: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
