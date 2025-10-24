19c7cbea1f1fb5d25c2052847f6f6199bc6909c5
TI <uandersonferreiracosta@gmail.com>
2025-10-24 15:34:55 -0300

    feat: adicionando o quiz como entity no module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './user/users.module';
 import { Users } from './user/entities/users.entity';
 import { QuizzesModule } from './quizzes/quizzes.module';
+import { Quiz } from './quizzes/entities/quiz.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'dbQuizzy.sqlite',
-      entities: [Users],
+      entities: [Users, Quiz],
       synchronize: true,
     }),
     UsersModule,
