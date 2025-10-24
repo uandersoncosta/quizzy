bdb44cb57442a48ca8da6f4f8d72ecd37efcff21
TI <uandersonferreiracosta@gmail.com>
2025-10-24 15:11:56 -0300

    feat: adicionando no service o create quiz

--- src/quizzes/quizzes.service.ts
+++ src/quizzes/quizzes.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateQuizDto } from './dto/create-quiz.dto';
 import { UpdateQuizDto } from './dto/update-quiz.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Quiz } from './entities/quiz.entity';
+import { Repository } from 'typeorm';
+import { UsersService } from 'src/user/users.service';
 
 @Injectable()
 export class QuizzesService {
-  create(createQuizDto: CreateQuizDto) {
-    return 'This action adds a new quiz';
+  constructor(
+    @InjectRepository(Quiz)
+    private readonly quizRepository: Repository<Quiz>,
+    private readonly usersRepository: UsersService,
+  ) {}
+
+  async create(createQuizDto: CreateQuizDto) {
+    const { userId, title, category, difficulty, description } = createQuizDto;
+
+    const user = await this.usersRepository.findOne(userId);
+
+    if (!user) {
+      throw new ConflictException('Este usuário não existe.');
+    }
+
+    const novoQuiz = {
+      user,
+      title,
+      description,
+      category,
+      difficulty,
+      created_at: new Date(),
+    };
+
+    const quiz = await this.quizRepository.create(novoQuiz);
+    await this.quizRepository.save(quiz);
+    return quiz;
   }
 
   findAll() {
