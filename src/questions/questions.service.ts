b202c2d6c8075efd07646934ca177f25d7e38921
TI <uandersonferreiracosta@gmail.com>
2025-10-31 16:40:07 -0300

    feat: adicionando o create question no service

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,18 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 import { Repository } from 'typeorm';
 import { Question } from './entities/question.entity';
 import { QuizzesService } from 'src/quizzes/quizzes.service';
+import { Quiz } from 'src/quizzes/entities/quiz.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class QuestionsService {
   constructor(
+    @InjectRepository(Question)
     private readonly questionRepository: Repository<Question>,
-    private readonly quizRepository: QuizzesService,
+
+    @InjectRepository(Quiz)
+    private readonly quizRepository: Repository<Quiz>,
   ) {}
 
-  create(quizId: number, createQuestionDto: CreateQuestionDto[]) {}
+  async create(quizId: number, createQuestionDto: CreateQuestionDto[]) {
+    const quiz = this.quizRepository.findOne({ where: { id: quizId } });
+
+    if (!quiz) {
+      throw new NotFoundException('Quiz não encontrado!');
+    }
+
+    const question = createQuestionDto.map((question) => {
+      return this.questionRepository.create({
+        quiz: { id: quiz },
+        ...question,
+      });
+    });
+
+    return this.questionRepository.save(question);
+  }
 
   findAll(quizId: number) {
     return `This action returns all questions`;
