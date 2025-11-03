15498a2656153b88a2e6c5e90a449b1deee256f8
TI <uandersonferreiracosta@gmail.com>
2025-11-03 15:06:51 -0300

    fix: consertando o import do Quiz no questionsModule

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -3,10 +3,10 @@ import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Question } from './entities/question.entity';
-import { QuizzesModule } from 'src/quizzes/quizzes.module';
+import { Quiz } from 'src/quizzes/entities/quiz.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Question]), QuizzesModule],
+  imports: [TypeOrmModule.forFeature([Question, Quiz])],
   controllers: [QuestionsController],
   providers: [QuestionsService],
 })
