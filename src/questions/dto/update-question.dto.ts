94b991d338c13c66d4b9c6b94142259c0184fc2c
TI <uandersonferreiracosta@gmail.com>
2025-11-07 16:16:37 -0300

    feat: adicionando o update-questionDTO

--- src/questions/dto/update-question.dto.ts
+++ src/questions/dto/update-question.dto.ts
@@ -1,4 +1,61 @@
-import { PartialType } from '@nestjs/swagger';
+import {
+  PartialType,
+  OmitType,
+  ApiExtraModels,
+  ApiPropertyOptional,
+} from '@nestjs/swagger';
+import {
+  IsOptional,
+  ValidateNested,
+  IsObject,
+  IsString,
+  Length,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 import { CreateQuestionDto } from './create-question.dto';
 
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {}
+class PartialQuestionOptionsDto {
+  @ApiPropertyOptional({ example: 'Data Definition Language' })
+  @IsOptional()
+  @IsString()
+  @Length(3, 150)
+  A?: string;
+
+  @ApiPropertyOptional({ example: 'Data Manipulation Language' })
+  @IsOptional()
+  @IsString()
+  @Length(3, 150)
+  B?: string;
+
+  @ApiPropertyOptional({ example: 'Data Query Language' })
+  @IsOptional()
+  @IsString()
+  @Length(3, 150)
+  C?: string;
+
+  @ApiPropertyOptional({ example: 'Data Control Language' })
+  @IsOptional()
+  @IsString()
+  @Length(3, 150)
+  D?: string;
+}
+
+@ApiExtraModels(PartialQuestionOptionsDto)
+export class UpdateQuestionDto extends PartialType(
+  OmitType(CreateQuestionDto, ['options', 'question_type'] as const),
+) {
+  @ApiPropertyOptional({
+    type: PartialQuestionOptionsDto,
+    example: {
+      A: 'Data Definition Language',
+      B: 'Data Manipulation Language',
+      C: 'Data Query Language',
+      D: 'Data Control Language',
+    },
+  })
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => PartialQuestionOptionsDto)
+  options?: PartialQuestionOptionsDto;
+}
