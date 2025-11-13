b110a324633ab2239fb8373df908158203a4c8f6
TI <uandersonferreiracosta@gmail.com>
2025-11-13 15:49:17 -0300

    feat: adicionando o attemp e importando o users e quizzers no attempmodule

--- src/attemps/attemps.module.ts
+++ src/attemps/attemps.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AttempsService } from './attemps.service';
 import { AttempsController } from './attemps.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Attemp } from './entities/attemp.entity';
+import { UsersModule } from 'src/user/users.module';
+import { QuizzesModule } from 'src/quizzes/quizzes.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Attemp]), UsersModule, QuizzesModule],
   controllers: [AttempsController],
   providers: [AttempsService],
 })
