c034a5a08788ef90c289d70e73a55bcf37b3694d
TI <uandersonferreiracosta@gmail.com>
2025-11-14 16:24:24 -0300

    fix: corrigindo os paramentros do service e controller do attemp

--- src/attemps/attemps.controller.ts
+++ src/attemps/attemps.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AttempsService } from './attemps.service';
 import { CreateAttempDto } from './dto/create-attemp.dto';
 import { UpdateAttempDto } from './dto/update-attemp.dto';
@@ -8,27 +16,49 @@ export class AttempsController {
   constructor(private readonly attempsService: AttempsService) {}
 
   @Post()
-  create(@Body() createAttempDto: CreateAttempDto) {
-    return this.attempsService.create(createAttempDto);
+  create(
+    @Param('userId') userId: string,
+    @Param('quizId') quizId: string,
+    @Body() createAttempDto: CreateAttempDto,
+  ) {
+    return this.attempsService.create(userId, quizId, createAttempDto);
   }
 
   @Get()
-  findAll() {
-    return this.attempsService.findAll();
+  findAll(@Param('userId') userId: string, @Param('quizId') quizId: string) {
+    return this.attempsService.findAll(+userId, +quizId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.attempsService.findOne(+id);
+  @Get(':attemp')
+  findOne(
+    @Param('attemp') attemp: string,
+    @Param('userId') userId: string,
+    @Param('quizId') quizId: string,
+  ) {
+    return this.attempsService.findOne(+attemp, +userId, +quizId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAttempDto: UpdateAttempDto) {
-    return this.attempsService.update(+id, updateAttempDto);
+  @Patch(':attemp')
+  update(
+    @Param('attemp') attemp: string,
+    @Param('userId') userId: string,
+    @Param('quizId') quizId: string,
+    @Body() updateAttempDto: UpdateAttempDto,
+  ) {
+    return this.attempsService.update(
+      +attemp,
+      +userId,
+      +quizId,
+      updateAttempDto,
+    );
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.attempsService.remove(+id);
+  @Delete(':attemp')
+  remove(
+    @Param('attemp') attemp: string,
+    @Param('userId') userId: string,
+    @Param('quizId') quizId: string,
+  ) {
+    return this.attempsService.remove(+attemp, +userId, +quizId);
   }
 }
--- src/attemps/attemps.service.ts
+++ src/attemps/attemps.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAttempDto } from './dto/create-attemp.dto';
 import { UpdateAttempDto } from './dto/update-attemp.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Attemp } from './entities/attemp.entity';
+import { Repository } from 'typeorm';
+import { Quiz } from 'src/quizzes/entities/quiz.entity';
+import { Users } from 'src/user/entities/users.entity';
 
 @Injectable()
 export class AttempsService {
-  create(createAttempDto: CreateAttempDto) {
+  constructor(
+    @InjectRepository(Attemp)
+    private readonly attempRepository: Repository<Attemp>,
+
+    @InjectRepository(Quiz)
+    private readonly quizRepository: Repository<Quiz>,
+
+    @InjectRepository(Users)
+    private readonly userRepository: Repository<Users>,
+  ) {}
+
+  create(userId: number, quizId: number, createAttempDto: CreateAttempDto) {
     return 'This action adds a new attemp';
   }
 
-  findAll() {
+  findAll(userId: number, quizId: number) {
     return `This action returns all attemps`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} attemp`;
+  findOne(userId: number, quizId: number, attempId: number) {
+    return `This action returns a #${attempId} attemp`;
   }
 
-  update(id: number, updateAttempDto: UpdateAttempDto) {
-    return `This action updates a #${id} attemp`;
+  update(
+    userId: number,
+    quizId: number,
+    attempId: number,
+    updateAttempDto: UpdateAttempDto,
+  ) {
+    return `This action updates a #${attempId} attemp`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} attemp`;
+  remove(userId: number, quizId: number, attempId: number) {
+    return `This action removes a #${attempId} attemp`;
   }
 }
